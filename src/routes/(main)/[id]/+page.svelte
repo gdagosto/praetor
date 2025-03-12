3cb762d35982ad1b4303868c9694e2d0591a8fdc
Gabriel d`Agosto <gabriel.dagosto@engenharia.ufjf.br>
2025-03-12 17:58:47 -0300

    wip

--- src/routes/(main)/[id]/+page.svelte
+++ src/routes/(main)/[id]/+page.svelte
@@ -1,13 +1,70 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import '$lib/db/db.svelte';
+	import { db, type IDbStanding, type IDbTable } from '$lib/db/db.svelte';
+	import { stTournament, stTournaments } from '$lib/stores/tournament.svelte';
 	import { veknReport } from '$lib/utils/report';
+	import { importInto } from 'dexie-export-import';
+	import { saveAs } from 'file-saver-es';
+
+	type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+	let dataBlob: Blob;
 
 	function finals() {
 		console.log(veknReport());
+	}
+
+	async function exportData() {
+		// Export tournaments table only
+		const obj = {
+			tournaments: $state.snapshot(stTournament.info.current),
+			standings: $state.snapshot(stTournament.standings.current),
+			roundTables: $state.snapshot(stTournament.tables.current)
+		};
+
+		const blob = new Blob([JSON.stringify(obj)]);
+		dataBlob = blob;
+
+		return;
+
+		saveAs(blob, 'dexie-export.json');
+	}
+
+	function convertBlobToJSON(blob: Blob) {
+		const url = URL.createObjectURL(blob);
+		const xmlRequest = new XMLHttpRequest();
+		xmlRequest.open('GET', url, false);
+		xmlRequest.send();
+		return JSON.parse(xmlRequest.responseText);
+	}
+
+	async function importData(blob: Blob) {
+		const data = convertBlobToJSON(blob);
+
+		// 1. Remove all data currently in the DB
+		const id = data.tournaments.id;
+
+		db.standings.where('tournamentId').equals(id).delete();
+		data.standings.forEach((s: Optional<IDbStanding, 'id'>) => {
+			delete s['id'];
+			db.standings.add(s);
+		});
+
+		db.roundTables.where('tournamentId').equals(id).delete();
+		data.roundTables.forEach((s: Optional<IDbTable, 'id'>) => {
+			delete s['id'];
+			db.roundTables.add(s);
+		});
+
+		// db.tournaments.put(data.tournaments, id);
+
+		return;
 	}
 </script>
 
 Oi
 
 <Button onclick={finals}>Finals</Button>
+<Button onclick={exportData}>Export</Button>
+<Button onclick={() => importData(dataBlob)}>Import</Button>
---
+++ src/routes/import/file/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Input } from '$lib/components/ui/input';
+
+	let value = $state('');
+	let fileInput: HTMLInputElement | undefined = $state();
+
+	$inspect('value', value);
+
+	$effect(() => {
+		if (!fileInput) return;
+		const reader = new FileReader();
+		reader.addEventListener('load', () => {
+			console.log(reader.result);
+		});
+
+		reader.readAsText(fileInput.files[0], 'utf-8');
+	});
+</script>
+
+<Input type="file" bind:ref={fileInput} bind:value />
